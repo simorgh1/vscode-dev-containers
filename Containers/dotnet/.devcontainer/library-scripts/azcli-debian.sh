1afe64df07064975867db721fdb40d63b3ca1b1b
simorgh1 <bahi@hey.com>
2021-10-25 18:20:08 +0200

    Fixed azure-cli installation

--- Containers/dotnet/.devcontainer/library-scripts/azcli-debian.sh
+++ Containers/dotnet/.devcontainer/library-scripts/azcli-debian.sh
@@ -1,10 +1,6 @@
 set -e
 
-# Installing az beta, since az official does not support arm64 on mac m1
 apt-get update
-apt-get install python3-pip -y
-
-python3 -m pip install --upgrade pip
-pip install --extra-index-url https://azcliprod.blob.core.windows.net/beta/simple/ azure-cli
+apt-get install azure-cli -y
 
 echo "Done!"
