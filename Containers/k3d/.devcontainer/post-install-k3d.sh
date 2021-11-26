2f6ca42d6f068c89b7a377c53a4c95325166d3dd
simorgh1 <bahi@hey.com>
2021-11-26 15:53:32 +0100

    k3d added DevContainer to work with k3d clusters

---
+++ Containers/k3d/.devcontainer/post-install-k3d.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! which k3d > /dev/null 2>&1; then
+	echo "Installing k3d ..."
+	wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+fi
+
+DEMO=$(k3d cluster list -o json | jq '.[0].name // empty') 
+
+if [ "$DEMO" == "" ] ; then
+	echo "Creating demo cluster ..."
+	k3d cluster create demo
+fi
+
+if [ ! -d "~/.kube/" ]; then
+	mkdir ~/.kube/
+fi
+
+k3d kubeconfig get demo > ~/.kube/config
+kubectl cluster-info
---
+++ Containers/k3d/Readme.md
@@ -0,0 +1,19 @@
+### K3d
+
+K3s based on Docker engine inside Dev Container also known as k3d which is also compatible with arm64 MacBook Air M1 in addition to amd64 platform.
+
+By default, non-root user is enabled and could be configured in devcontainer.json
+
+### Installed extensions
+
+- Kubernetes
+- Docker
+- Pylance
+- Python
+- GitLens
+- GitHistory
+- Prettier
+- Code Spell Checker
+- Fix Json
+- ESLint
+- Material Icon Theme
